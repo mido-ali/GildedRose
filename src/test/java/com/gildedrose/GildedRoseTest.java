70e1a1f937992511cd63b0821df086bec68f2bad
Mido <mahmoud.ali@devsquads.com>
2022-09-07 00:13:25 +0200

    remove un nedded test and cover twice and negative quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,6 +15,20 @@ class GildedRoseTest {
     }
 
     @Test
+    void onceTheSellByDateHasPassedQualityDegradesTwiceAsFast() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 0, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].toString(), new Item("Elixir of the Mongoose", -1, 48).toString());
+    }
+    @Test
+    void qualityOfAnItemIsNeverNegative() {
+        Item[] items = new Item[]{new Item("Elixir of the Mongoose", 5, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].toString(), new Item("Elixir of the Mongoose", 4, 0).toString());
+    }
+    @Test
     void dexterityVestItemQualityDecreasesByNextDay() {
         Item[] items = new Item[]{new Item("+5 Dexterity Vest", 10, 20)};
         GildedRose app = new GildedRose(items);
@@ -39,27 +53,26 @@ class GildedRoseTest {
     }
 
     @Test
-    void qualityOfAnItemIsNeverMoreThan50() {
-        Item[] items = new Item[]{new Item("+5 Dexterity Vest", 10, 50)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(app.items[0].toString(), new Item("+5 Dexterity Vest", 9, 50).toString());
-    }
-
-    @Test
     void qualityOfAgedBrieItemIsNeverMoreThan50() {
         Item[] items = new Item[]{new Item("Aged Brie", 2, 50)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(app.items[0].toString(), new Item("Aged Brie", 1, 50).toString());
     }
+    @Test
+    void qualityOfBackstagePassesItemIsNeverMoreThan50() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 12, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].toString(), new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50).toString());
+    }
 
     @Test
     void sulfurasLegendaryItemNeverSoldOrDecreasesInQuality() {
-        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 40)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(app.items[0].toString(), new Item("Sulfuras, Hand of Ragnaros", 0, 80).toString());
+        assertEquals(app.items[0].toString(), new Item("Sulfuras, Hand of Ragnaros", 0, 40).toString());
     }
 
     @Test
